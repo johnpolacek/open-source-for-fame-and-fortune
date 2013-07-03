035857a4e173cfc5570b2b87dd46c5ea3082fc7c
johnpolacek <johnpolacek@gmail.com>
2013-07-03 08:44:10 -0500

    update tweendeck to use tweento instead of reverse

--- js/libs/tweendeck.js
+++ js/libs/tweendeck.js
@@ -1,6 +1,6 @@
 /*
 	TweenDeck - For making animated presentation decks with Greensock
-	by John Polacek (@johnpolacek)
+	by John Polacek (@johnpolacek) - with some help from @greensock himself
 
 	Powered by the Greensock Tweening Platform
 	http://www.greensock.com
@@ -14,35 +14,58 @@
 	$.tweendeck = function(timeline, options) {
 
 		var tweendeck = this;
+		var positions = [];
+		var positionIndex = 1;
+		var timeScale = 0;
 		var defaults = {
-
+			allowSkip:true
 		};
 		tweendeck.settings = $.extend({}, defaults, options);
 
-		function pauseTimeline() { timeline.pause(); }
-
+		// Record the start time of each child tween/timeline
 		$.each(timeline.getChildren(false), function() {
-			this.eventCallback('onComplete', pauseTimeline).eventCallback('onReverseComplete', pauseTimeline);
+			positions.push(this.startTime());
 		});
+
+        timeline.tweenTo(positions[1]); //animate to the first deck
 
 		// Keyboard events
 		$(document).on('keydown', function(e){
 			// down/right arrow, pagedown, space = play forward
-			if (e.keyCode === 34 || e.keyCode === 39 || e.keyCode === 32 || e.keyCode === 40) {
-				timeline.play();
+			if ((e.keyCode === 34 || e.keyCode === 39 || e.keyCode === 32 || e.keyCode === 40) && positionIndex < positions.length) {
+				tweendeck.next();
 			}
 			// up/left arrow, pageup = rewind
-			else if (e.keyCode === 33 || e.keyCode === 37 || e.keyCode === 38) {
-				timeline.reverse();
+			else if ((e.keyCode === 33 || e.keyCode === 37 || e.keyCode === 38) && positionIndex > 1) {
+				tweendeck.prev();
 			}
 		});
 
+		// Move the playhead to the appropriate position (based on the index)
+		function tweenTo(i) {
+			if (tweendeck.settings.allowSkip) {
+				timeScale++; //speed up if the user keeps pushing the button.
+			} else if (timeScale !== 0) {
+				// If the timeScale isn't 0, that means we're mid-tween, and since allowSkip is false, we should ignore the request.
+				return;
+			} else {
+				timeScale = 1;
+			}
+			positionIndex = i;
+			// Tween the "time" (playhead) to the new position using a linear ease. We could have used timeline.tweenTo() if we knew the timeline would always be a TimelineMax, but this code makes it compatible with TimelineLite too.
+			TweenLite.to(timeline, Math.abs(positions[i] - timeline.time()), {time:positions[i], ease:Linear.easeNone, onComplete:function() {
+					// Reset the timeScale when the tween is done
+					timeScale = 0;
+				}
+			}).timeScale(timeScale);
+		}
+
 		// Public Functions
 		tweendeck.next = function() {
-			timeline.play();
+			tweenTo(positionIndex+1);
 		};
 		tweendeck.prev = function() {
-			timeline.reverse();
+			tweenTo(positionIndex-1);
 		};
 
 		return tweendeck;
